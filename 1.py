ad7069673e615687e5a83be4c772531cec39088a
sam2776 <smikheeva27@gmail.com>
2021-11-18 19:14:04 +0400

    четвертое version

--- 1.py
+++ 1.py
@@ -2,6 +2,7 @@ def main():
     print('My first git program')
     print("6b4d3e7a233ed2d925ed2cd19f5866ce910f033a")
     print("111111")
+    print('kjkjk')
 
 if __name__ == '__main__':
     main()
